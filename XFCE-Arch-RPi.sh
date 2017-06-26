92c1cabcb0bf569f12a3ea06a8361cd4a4aaee66
ReDemoNBR <alanderson_laird@yahoo.com.br>
2017-06-26 20:07:02 -0300

    Update XFCE-Arch-RPi.sh
    added audio, base packages, dhcpcd, swap and replaced slim with lightdm

--- XFCE-Arch-RPi.sh
+++ XFCE-Arch-RPi.sh
@@ -1,16 +1,33 @@
 echo "Run this script as root only."
 echo "You can hit Ctrl+c in next 10 seconds if you don't want to continue."
 sleep 10
-pacman-key --init 
+# initialize pacman keys
+pacman-key --init
+# update system
 pacman -Syu --noconfirm
+# install base packages
+pacman -S --noconfirm --needed base base-devel yajl wget
+# install X.org
 pacman -S --noconfirm xorg-xinit xorg-server xorg-server-utils xterm
-pacman -S --noconfirm xfce4
-pacman -S --noconfirm mesa xf86-video-fbdev xf86-video-vesa
-pacman -S --noconfirm slim
-systemctl enable slim.service
+# install Xfce4 desktop environment
+pacman -S --noconfirm xfce4 xfce4-goodies
+# install video and audio
+pacman -S --noconfirm mesa xf86-video-fbdev xf86-video-vesa pulseaudio pulseaudio-alsa
+# install LightDM display manager
+pacman -S --noconfirm lightdm lightdm-gtk-greeter
+systemctl enable lightdm.service
 systemctl enable graphical.target
+# enable DHCPCD
+systemctl enable dhcpcd
 
-curl https://raw.github.com/adimania/arch-desktop-environments/master/xinitrc >> ~/.xinitrc
-curl https://raw.github.com/adimania/arch-desktop-environments/master/bash_profile >> ~/.bash_profile
+# create 2GB swapfile in /swapfile
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo /swapfile none swap defaults 0 0 | tee -a /etc/fstab
+
+curl https://raw.github.com/ReDemoNBR/arch-desktop-environments/master/xinitrc >> ~/.xinitrc
+curl https://raw.github.com/ReDemoNBR/arch-desktop-environments/master/bash_profile >> ~/.bash_profile
 
 reboot
